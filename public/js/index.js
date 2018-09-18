8abfac347c712a1d48aff55054b3af23f3ed20fb
Hassan Irshad <hassanansari17@live.com>
2018-09-18 15:14:20 +0500

    Scroll to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+    //selectors
+    var messages = jQuery('#message');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop +newMessageHeight +lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
+
 socket.on('connect', function() {
     console.log('Connected to server');
 
@@ -18,6 +35,7 @@ socket.on('newMessage', function(msg) {
         createdAt: formattedTime
     });
     jQuery('#message').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -29,6 +47,7 @@ socket.on('newLocationMessage', function (message) {
         url: message.url
     });
     jQuery('#message').append(html);
+    scrollToBottom();
 });
 
 
