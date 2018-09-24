e6c48d4133c4b1f0108dcf4a55cae3916a1e2b01
Hassan Irshad <hassanansari17@live.com>
2018-09-24 16:58:07 +0500

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,6 @@ jQuery('#message-form').on('submit', function(e) {
 
     var messageTextbox = jQuery('[name=message]');
     socket.emit('createMsg', {
-        from: 'User',
         text: messageTextbox.val(),
     }, function() {
         messageTextbox.val('');
